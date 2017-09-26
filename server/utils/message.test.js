5bd7339307c0381c1488e577cd578840bfa3bfdf
djbedford <david.bedford@netsend.com>
2017-09-26 16:10:16 +0100

    Add geolocation support via geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -9,6 +9,18 @@ describe('generateMessage', () => {
     expect(message).toInclude({
       from: 'David',
       text: 'This is a test',
+    });
+    expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var message = generateLocationMessage('Admin', 5, 21);
+
+    expect(message).toInclude({
+      from: 'Admin',
+      url: 'https://www.google.com/maps?q=5,21'
     });
     expect(message.createdAt).toBeA('number');
   });
